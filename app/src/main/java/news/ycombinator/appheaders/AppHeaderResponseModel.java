9347050e6e0c679ed6e87b1bd2ae8e473d16b1d1
Thong <tranvanthong1991@gmail.com>
2017-06-12 23:15:38 +0700

    Them

--- app/src/main/java/news/ycombinator/appheaders/AppHeaderResponseModel.java
+++
@@ -1,9 +0,0 @@
-package news.ycombinator.appheaders;
-
-public class AppHeaderResponseModel {
-
-    //---> Status from server
-    public static final String API_RESPONSE_STATUS = "status";
-    public static final String API_STATUS_FAIL = "FAIL";
-    public static final String API_STATUS_OK = "OK";
-}
--- app/src/main/java/news/ycombinator/appheaders/AppHeaderkey.java
+++
@@ -1,8 +0,0 @@
-package news.ycombinator.appheaders;
-
-public class AppHeaderkey {
-
-    public static final String STORY = "STORY";
-    public static final String COMMENT = "COMMENT";
-
-}
